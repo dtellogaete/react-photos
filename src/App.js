31b21a5a76a0e7d05db42b8a96e00b8839c165cb
Daniel Tello Gaete <dtellogaete@gmail.com>
2023-01-14 00:41:58 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title= "Galería de Imágenes"/>
+    </div>
+  );
+}
+
+export default App;
